dd95f087bbde811fa71e386b2858596d8cdda36b
phbarbudo <paulohss.souza@gmail.com>
2023-06-27 06:53:55 -0300

    Challenge: Place the IMDB API key somewhere outside the code as a configuration file (e.g., a .properties file) or an environment variable

--- src/com/paulohenrique/Main.java
+++ src/com/paulohenrique/Main.java
@@ -1,6 +1,7 @@
 package com.paulohenrique;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,14 +10,26 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+    	
+    	Properties connectionProperties = new Properties();
+    	try {
+			FileInputStream fileInputStream = 
+					new FileInputStream("C:\\Users\\paulo\\OneDrive\\Programação\\Exercícios Oracle ONE\\BackEndJava\\bestMoviesImdb\\src\\com\\paulohenrique\\connection.properties");
+			connectionProperties.load(fileInputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	String userKeyApiImdb = connectionProperties.getProperty("userKeyApiImdb");
+    	
         /*
-          Conexion from HTTP of IMDB 250 best movies.
+          Connection from HTTP of IMDB 250 best movies.
          */
 
-        String url250BestMovies = "https://imdb-api.com/en/API/Top250Movies/k_45r7fn7q";
+        String url250BestMovies = "https://imdb-api.com/en/API/Top250Movies/" + userKeyApiImdb;
         URI uri250BestMovies = URI.create(url250BestMovies);
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(uri250BestMovies).GET().build();
@@ -36,6 +49,7 @@ public class Main {
          */
 
         movieList.forEach((movie) -> {
+        	System.out.println("250 melhores filmes:");
             System.out.println(movie.get("title"));
             System.out.println(movie.get("image"));
             System.out.println(movie.get("imDbRating"));
